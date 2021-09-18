0050f3be7cf0ff22e7fae58225ba19b10ee8c6b1
NullVoxPopuli <LPSego3+dev@gmail.com>
2021-09-18 11:58:40 -0400

    Delete more files

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,20 +4,8 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function (defaults) {
   let app = new EmberApp(defaults, {
-    // Theme options are coy, dark, funky, okaidia, solarizedlight, tomorrow,
-    // and twilight. Remove the `theme` attribute to use the default theme.
-    'ember-prism': {
-      theme: 'tomorrow',
-      components: [
-        'handlebars',
-        'markup-templating', // handlebars
-        'bash', // shell
-      ],
-      plugins: ['line-numbers'],
-    },
-
     prember: {
-      urls: ['/'],
+      urls: ['/', '/docs/'],
     },
   });
 
