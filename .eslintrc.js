6fb4cdc1d62b206bcd64d2d5db859d557b8932f2
NullVoxPopuli <LPSego3+dev@gmail.com>
2021-09-19 10:45:15 -0400

    Update lint config to support Node ESModules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,21 @@
 'use strict';
 
 const { configs } = require('@nullvoxpopuli/eslint-configs');
+const { baseConfig, baseModulesConfig } = require('@nullvoxpopuli/eslint-configs/configs/node');
 
-module.exports = configs.ember();
+let ember = configs.ember();
+
+module.exports = {
+  ...ember,
+  overrides: [
+    ...ember.overrides,
+    {
+      files: ['lib/**/*.js', 'lib/**/*.cjs'],
+      ...baseConfig,
+    },
+    {
+      files: ['lib/**/*.mjs'],
+      ...baseModulesConfig,
+    },
+  ],
+};
