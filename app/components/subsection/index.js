67f35abd7d4cec8288c76d37661b5808fde41d0a
NullVoxPopuli <LPSego3+dev@gmail.com>
2021-12-01 08:15:09 -0500

    Add let example with some supporting components to make writing out references a bit easier in the future

--- app/components/subsection/index.js
+++ app/components/subsection/index.js
@@ -3,4 +3,6 @@ import { inject as service } from '@ember/service';
 
 export default class GuideSectionSubsectionComponent extends Component {
   @service intl;
+
+  t = (key, options) => this.intl.t(key, { ...options, htmlSafe: true });
 }
